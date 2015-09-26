9a52589796f1bf103ec6e86d59dc8ddf4db73908
Jeannie <jeannie@kickpunchcreative.com>
2015-09-26 15:35:12 -0500

    requests

--- controllers/contacts.rb
+++ controllers/contacts.rb
@@ -21,3 +21,49 @@ end
 get "/question-final" do
 erb :"question-final"
 end
+
+post "/contacts" do
+  
+  Pony.mail ( {
+        :to => 'jeannie@kickpunchcreative.com, jeannie@omaha.dog',
+        :from => "jeannie@kickpunchcreative.com",
+        :subject => "Omaha.Dog Submission :#{params[:name]}",
+        :body=> "business name: #{params[:name]}",
+        :via => :smtp,
+        :via_options => {  
+          :address   => 'smtp.gmail.com',  
+          :port   => '587',  
+          :enable_starttls_auto => true,
+          :user_name   => 'jeannie@kickpunchcreative.com',  
+          :password   => 'genie1983',  
+          :authentication   => :plain,   
+          :domain => "kickpunchcreative.com"
+      }
+      })
+    redirect "#thanks" #should redirect to login after creating contact.
+ 
+ 
+end
+
+post "/stickers" do
+  
+  Pony.mail ( {
+        :to => 'jeannie@kickpunchcreative.com, jeannie@omaha.dog',
+        :from => "jeannie@kickpunchcreative.com",
+        :subject => "Omaha.Dog Sticker Request :#{params[:name]}",
+        :body=> "business name: #{params[:name]} business address: #{params[:address]}",
+        :via => :smtp,
+        :via_options => {  
+          :address   => 'smtp.gmail.com',  
+          :port   => '587',  
+          :enable_starttls_auto => true,
+          :user_name   => 'jeannie@kickpunchcreative.com',  
+          :password   => 'genie1983',  
+          :authentication   => :plain,   
+          :domain => "kickpunchcreative.com"
+      }
+      })
+    redirect "#thanks-two" #should redirect to login after creating contact.
+ 
+ 
+end
--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,23 @@
 var score = 0;
 var counter=0
 
-var questionOne = document.getElementById("q1o1"); //
+var submit = document.getElementById("submit");
+var thanks = document.getElementById("thanks");//
 // var q1o2 = document.getElementById("q1o2"); //
 // var q1o3 = document.getElementById("q1o3"); //
 // var q1o4 = document.getElementById("q1o4"); //
 // var result = document.getElementById("result");//
 
 
-questionOne.onclick = tally;
+submit.onclick = thanksmessage;
 // q1o2.onclick = process_answer_submission2;
 // q1o3.onclick = process_answer_submission3;
 // q1o4.onclick = process_answer_submission4;
 
 
-var tally = function() {
- score++;
- counter++;
- finalScore();
+var thanksmessage = function() {
+  thanks.innerHTML = "thanks";
+
 }
 
 // var process_answer_submission2 = function() {
@@ -33,7 +33,4 @@ var tally = function() {
 // }
 
 
-var finalScore = function() {
-  var score_announce = "You answered " + score + " out of 4 questions correct";
-  result.innerHTML = score_announce;// feeding total_result div with score announcement
-};
+
