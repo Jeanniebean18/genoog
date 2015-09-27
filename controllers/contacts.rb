0c058e0cec4dc23a0a35ef72ce3855ce885560e4
Jeannie <jeannie@kickpunchcreative.com>
2015-09-27 15:15:23 -0500

    sd

--- controllers/contacts.rb
+++ controllers/contacts.rb
@@ -2,25 +2,9 @@ get "/" do
 erb :"index"
 end
 
-get "/question-one" do
-erb :"question-one"
-end
 
-get "/question-two" do
-erb :"question-two"
-end
 
-get "/question-three" do
-erb :"question-three"
-end
 
-get "/question-four" do
-erb :"question-four"
-end
-
-get "/question-final" do
-erb :"question-final"
-end
 
 post "/contacts" do
   
